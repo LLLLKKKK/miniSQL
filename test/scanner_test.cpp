3d4c71fc5cfe871de9193447bab076dc279e5c61
LLLLKKKK <kan.liu.229@gmail.com>
2013-03-25 16:31:31 -0700

    add int convert

--- test/scanner_test.cpp
+++ test/scanner_test.cpp
@@ -4,6 +4,7 @@
 #define BOOST_TEST_MAIN
 
 #include <iostream>
+#include <cstdlib>
 #include <boost/test/included/unit_test.hpp>
 #include "../scanner.hpp"
 
@@ -30,13 +31,21 @@ BOOST_AUTO_TEST_CASE( speedTest )
 {
 	SQLScanner scanner("100wtest.sql");
 	int count = 0;
+	int a1 = 0;
+	float a2 = 0;
 	Token token = scanner.nextToken();
 
 	while (token != NULLTOKEN)
 	{
 		if (token == TERMINATOR) count++;
 		token = scanner.nextToken();
+		if (token == INTEGER)
+			a1 += atoi(scanner.getTokenBuffer());
+		else if (token == FLOAT)
+			a2 += atof(scanner.getTokenBuffer());
 	}
+
+	cout << a1 << a2;
 	BOOST_REQUIRE_EQUAL( count, 1000000 );
 }
 
